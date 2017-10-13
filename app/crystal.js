a73921b0f325287cf846ff13cd9ba75a630cffbf
jkrovitz <jkrovitz@macalester.edu>
2017-10-13 13:54:08 -0500

    added files   and tested out rotating other shapes.

--- app/crystal.js
+++ app/crystal.js
@@ -1,16 +1,2 @@
-var rect;
-var side2; 
-var side3; 
-tool.minDistance = 5;
-
-var rect = new Path.Rectangle(new Point(50, 50), new Size(50, 50));
-rect.style = {
-	fillColor: 'blue',
-};
-
-function onMouseMove(event) {
-    rect.rotate(20);    
-}
-
-
+//This is our main code file. 
 
---
+++ app/hexagon.js
@@ -0,0 +1,30 @@
+// Create a Paper.js Path to draw a line into it:
+var hexagon = new Path();
+// Color our path black
+hexagon.strokeColor = 'black';
+
+// How many points do we want our object to have
+var points = 6;
+// How large should it be
+var radius = 20;
+// 0 to 2PI is a circle, so divide that by the number of points
+// in our object and that's how many radians we should put a new
+// point in order to draw the shape
+var angle = ((2 * Math.PI) / points);
+
+// For as many vertices in the shape, add a point
+for(i = 0; i <= points; i++) {
+
+  // Add a new point to the object
+  hexagon.add(new Point(
+    // Radius * Math.cos(number of radians of the point) is the x position
+    radius * Math.cos(angle * i), 
+    // And the same thing with Math.sin for the y position of the point
+    radius * Math.sin(angle * i)
+  ));
+}
+
+// Offset the shape so it's fully displayed on the canvas
+hexagon.position.x += 100;
+hexagon.position.y += 100;
+
